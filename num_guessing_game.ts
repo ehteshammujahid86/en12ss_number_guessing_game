f194426ab33c1a6f8c3ab42b7d9695eae29e2372
ehteshammujahid86 <ehteshammujahid86@gmail.com>
2024-06-09 13:28:13 +0500

    Add files via upload

---
+++ num_guessing_game.ts
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+
+import { log } from "console";
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+const answer = await inquirer.prompt([
+  {
+    name: "guessNumber",
+    type: "number",
+    message: "Please select a number between 1 ~ 5\n",
+  },
+]);
+
+function guessingNum(Num: number) {
+  if (randomNumber === answer.guessNumber) {
+    console.log("You Won");
+    return;
+  } else {
+    console.log("Please Guess Again");
+  }
+}
+guessingNum(answer.prompt);
